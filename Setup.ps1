2c730df68e47170a75185392106ed5d2c943bde6
Finn Beckitt-Marshall <finn@pictura.co.uk>
2024-06-09 18:59:59 +0100

    Added code to select the appropriate drive

--- Setup.ps1
+++ Setup.ps1
@@ -3,6 +3,8 @@ Write-Host "FBM's Rockbox Scripts"
 Write-Host "2024 Finn Beckitt-Marshall"
 Write-Host "Licensed under the GNU General Public License - see LICENSE for more details."
 
+Write-Host "`nEnsure your Rockbox music player is connected to your PC."
+
 $setupScrobbling = $false
 $scrobble = Read-Host "`nDo you want to set up scrobbling to Last.fm? (Y/N)"
 Switch ($scrobble)
@@ -10,7 +12,28 @@ Switch ($scrobble)
     "Y" {$setupScrobbling = $true}
     "N" {Write-Host "Scrobbling setup skipped."}
 }
+
 # Set up scrobbling
 if ($setupScrobbling){
     & $PSScriptRoot/Setup/ScrobbleSetup.ps1
 }
+
+# Ask the user to select the drive their Rockbox player is connected to
+$drives = Get-WmiObject -Class Win32_LogicalDisk | Select-Object DeviceID, VolumeName
+$driveIndex = 1
+
+Write-Host "Available drives:"
+$drives | ForEach-Object {
+    Write-Host "$driveIndex. $($_.DeviceID) - $($_.VolumeName)"
+    $driveIndex++
+}
+
+$selectedDriveIndex = Read-Host "Enter the number corresponding to the drive:"
+$selectedDrive = $drives[$selectedDriveIndex - 1]
+
+if ($selectedDrive) {
+    Write-Host "You selected drive $($selectedDrive.DeviceID)"
+} else {
+    Write-Host "Invalid selection."
+}
+
